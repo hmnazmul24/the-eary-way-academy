ef6dc458bc821e9c493e91ae97cfb743f98534eb
shoubj-ahmed <codegreen19s@gmail.com>
2025-06-02 09:03:25 +0600

    removed analytics page

--- app/(admin)/admin/analytics/page.tsx
+++ app/(admin)/admin/analytics/page.tsx
@@ -1,59 +1,74 @@
-"use client";
+// "use client";
+
+// import React from "react";
+// import BranchGrouthChart from "../../_charts/BranchGrouthChart";
+// import StudentGrouthChart from "../../_charts/StudentGrouthChart";
+// import RevenueChart from "../../_charts/RevenueChart";
+// import { useQuery } from "@tanstack/react-query";
+// import { admin_branch_grouth } from "@/actions/adminAnalytics";
+
+// const AdmimPage = () => {
+//   const { data, isPending } = useQuery({
+//     queryKey: ["admin_growth"],
+//     queryFn: async () => await admin_branch_grouth(),
+//   });
+
+//   if (isPending) {
+//     return <div>Loading...</div>;
+//   }
+//   if (!data) {
+//     return null;
+//   }
+
+//   let {
+//     branchCount,
+//     dataArr,
+//     notVarified,
+//     paidStudentsCount,
+//     studentCount,
+//     studentProgressArr,
+//     unpaidStudentCount,
+//     revenue,
+//   } = data;
+
+//   let formateStudentData = studentProgressArr
+//     ?.slice()
+//     .reverse()
+//     .map((item, i) => ({
+//       count: item.count,
+//       week: `${i} week`,
+//     }));
+//   return (
+//     <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+//       <BranchGrouthChart
+//         data={dataArr!}
+//         branchCount={branchCount!}
+//         notVarified={notVarified!}
+//       />
+//       <StudentGrouthChart
+//         data={formateStudentData}
+//         paid={paidStudentsCount!}
+//         total={studentCount!}
+//         unpaid={unpaidStudentCount!}
+//       />
+//       <RevenueChart revenue={revenue!} />
+//     </div>
+//   );
+// };
+
+// export default AdmimPage;
 
 import React from "react";
-import BranchGrouthChart from "../../_charts/BranchGrouthChart";
-import StudentGrouthChart from "../../_charts/StudentGrouthChart";
-import RevenueChart from "../../_charts/RevenueChart";
-import { useQuery } from "@tanstack/react-query";
-import { admin_branch_grouth } from "@/actions/adminAnalytics";
 
-const AdmimPage = () => {
-  const { data, isPending } = useQuery({
-    queryKey: ["admin_growth"],
-    queryFn: async () => await admin_branch_grouth(),
-  });
-
-  if (isPending) {
-    return <div>Loading...</div>;
-  }
-  if (!data) {
-    return null;
-  }
-
-  let {
-    branchCount,
-    dataArr,
-    notVarified,
-    paidStudentsCount,
-    studentCount,
-    studentProgressArr,
-    unpaidStudentCount,
-    revenue,
-  } = data;
-
-  let formateStudentData = studentProgressArr
-    ?.slice()
-    .reverse()
-    .map((item, i) => ({
-      count: item.count,
-      week: `${i} week`,
-    }));
+const AdminAnalyticsPage = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-      <BranchGrouthChart
-        data={dataArr!}
-        branchCount={branchCount!}
-        notVarified={notVarified!}
-      />
-      <StudentGrouthChart
-        data={formateStudentData}
-        paid={paidStudentsCount!}
-        total={studentCount!}
-        unpaid={unpaidStudentCount!}
-      />
-      <RevenueChart revenue={revenue!} />
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus saepe
+      fugit possimus incidunt, praesentium dignissimos. Mollitia expedita, magni
+      velit ad ipsam corrupti minima aliquid pariatur optio ab quidem, veniam
+      soluta.
     </div>
   );
 };
 
-export default AdmimPage;
+export default AdminAnalyticsPage;
